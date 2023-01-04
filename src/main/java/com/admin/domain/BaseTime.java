78f19325c06bb5b912c5e01b809c8df40ee40f27
venh <conf312@naver.com>
2023-01-04 15:35:35 +0900

    [modify] date format

--- src/main/java/com/admin/domain/BaseTime.java
+++ src/main/java/com/admin/domain/BaseTime.java
@@ -25,7 +25,7 @@ public abstract class BaseTime {
     private LocalDateTime modifyTime;
 
     public static String toStringDateTime(LocalDateTime localDateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         return Optional.ofNullable(localDateTime)
                 .map(formatter::format)
                 .orElse("");
