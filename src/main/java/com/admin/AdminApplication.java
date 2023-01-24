f97220ab0dcca33bfb82a465e26d630771e824e7
venh <conf312@naver.com>
2023-01-24 12:49:13 +0900

    [add] redis session clustering

--- src/main/java/com/admin/AdminApplication.java
+++ src/main/java/com/admin/AdminApplication.java
@@ -3,9 +3,11 @@ package com.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@EnableJpaAuditing
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableRedisHttpSession
 public class AdminApplication {
 
 	public static void main(String[] args) {
